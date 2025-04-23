094f6badfb385422f169bc9189dd21fd15117113
Muhammed Aslam Shah S <muhammedaslamshahsofficial@gmail.com>
2025-04-23 14:24:05 -0700

    added gitignore

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
 const http = require("http");
 const fs = require("fs");
-const path = require("path");
+const path = require('path');
 
-const Server = http.createServer((req, res) => {
-    if (req.url === "/") {
-        fs.readFile(path.join(__dirname, "views", "index.html"), "utf-8", (err, data) => {
-            if (err) throw err;
+const server = http.createServer((req,res)=>{
 
-            res.writeHead(200, { "content-type": "text/html" });
-            res.end(data);
-        });
-    }
+    const requestedPage = req.url === '/' ? "index.html" : `${req.url}.html`;
     
+    const filePath = path.join(__dirname, "views", requestedPage);
+
+    fs.readFile(filePath, "utf-8", (err, data)=>{
+
+
+        if (err) {
+            fs.readFile(path.join(__dirname, "views", "error404.html"), "utf-8", (err404, errorPage) => {
+                
+                res.writeHead(404, {"content-type": "text/html"});
+                res.end(err404 ? "<h1>404 Not Found</h1>" : errorPage);
+            });
+        }else{
+            res.writeHead(200, {"content-type": "text/html"});
+            res.end(data);
+        }
+
+       
+
+
+    });
+
+
 });
+
 
 const PORT = process.env.PORT || 3001;
 
-Server.listen(PORT, () => console.log(`Server running on ${PORT}`));
+server.listen(PORT, ()=> console.log(`Server running... on ${PORT}`));
